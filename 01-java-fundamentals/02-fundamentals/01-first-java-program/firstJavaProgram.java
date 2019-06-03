921f900476716dcb12d1c8eeb7ef90a57bf46b60
Kent Hervey <kent.hervey@gmail.com>
2019-06-03 14:51:22 -0500

    First Java Program complete

---
+++ 01-java-fundamentals/02-fundamentals/01-first-java-program/firstJavaProgram.java
@@ -0,0 +1,14 @@
+
+public class firstJavaProgram {
+
+	public static void main(String[] args) {
+		String outPutString = "";
+		outPutString = "My name is Kent Hervey";
+		outPutString += "\nI am not too many years old";
+		outPutString += "\nMy hometown is Texas";
+		System.out.println(outPutString);
+				
+
+	}
+
+}
