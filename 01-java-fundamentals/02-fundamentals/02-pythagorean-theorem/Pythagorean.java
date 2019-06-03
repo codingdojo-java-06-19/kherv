a95eba4309a97d35367b4d1e90893a0462927d9d
Kent Hervey <kent.hervey@gmail.com>
2019-06-03 15:19:12 -0500

    Pythagorean completed

---
+++ 01-java-fundamentals/02-fundamentals/02-pythagorean-theorem/Pythagorean.java
@@ -0,0 +1,9 @@
+
+public class Pythagorean {
+
+	public double calculatHypotenuse(int legA, int legB) {
+		return Math.sqrt(Math.pow(legA, 2) + Math.pow(legB, 2));
+
+	}
+
+}
---
+++ 01-java-fundamentals/02-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,14 @@
+
+public class PythagoreanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pythagorean pyth = new Pythagorean();
+				
+		double result = pyth.calculatHypotenuse( 3,  4);
+		System.out.println("Hypotenuse is:  " + result);
+				
+
+	}
+
+}
