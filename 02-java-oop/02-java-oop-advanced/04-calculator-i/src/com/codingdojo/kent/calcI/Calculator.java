da262d1b2ed096a9fd04b3c407408495d2cc86ee
Kent Hervey <kent.hervey@gmail.com>
2019-06-05 15:13:29 -0500

    calculator I complete

---
+++ 02-java-oop/02-java-oop-advanced/04-calculator-i/src/com/codingdojo/kent/calcI/Calculator.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.codingdojo.kent.calcI;
+
+public class Calculator {
+
+	private double operandOne;
+	private String operation;
+	private double operandTwo;
+	private double results;
+
+	public Calculator() {
+		this.results = -55.2;
+
+	}
+
+	public void setOperandOne(double inOper1) {
+		this.operandOne = inOper1;
+	}
+
+	public void setOperation(String inOperation) {
+		this.operation = inOperation;
+	}
+
+	public void setOperandTwo(double inOper2) {
+		this.operandTwo = inOper2;
+	}
+
+	public void performOperation() {
+		if (this.operation == "+") {
+			this.results = this.operandOne + this.operandTwo;
+		}
+	}
+
+	public void getResults() {
+		System.out.println("Result is:  " + this.results);
+	}
+
+}
---
+++ 02-java-oop/02-java-oop-advanced/04-calculator-i/src/com/codingdojo/kent/calcI/UseCalculator.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.codingdojo.kent.calcI;
+
+public class UseCalculator implements java.io.Serializable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calculator firstCalc = new Calculator();
+
+		firstCalc.setOperandOne(10.5);
+
+		firstCalc.setOperation("+");
+
+		firstCalc.setOperandTwo(5.2);
+
+		firstCalc.performOperation();
+
+		firstCalc.getResults();
+
+	}
+
+}
