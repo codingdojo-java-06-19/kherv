c402b381e5e7c8a653cb7d5b5731b82c826406c6
Kent Hervey <kent.hervey@gmail.com>
2019-06-10 14:14:13 -0500

    Great Number Game minimum working version

---
+++ 03-java-on-the-web/01-java-on-the-web/05-great-number-game/WebContent/WEB-INF/index.jsp
@@ -0,0 +1,70 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    
+    
+        <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %> 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Great Number Game</title>
+<link rel="stylesheet" href="/WEB-INF/styles.css" />
+<style>
+<style>
+     <%@ include file="/WEB-INF/styles.css"%>
+</style>
+
+
+
+
+</head>
+<body>
+
+<h1>Welcome to the Great Number Game!</h1>
+<h2>I am thinking of a number between 1 and 100</h2>
+<h3>Take a guess!</h3>
+
+		<% if ((session.getAttribute("didGuess") == "yes") && (session.getAttribute("guessResult") == "1" )){ %>
+			<div class="alert red">
+				<p>Too high!</p>
+			</div>
+		<% } %>
+		
+				<% if ((session.getAttribute("didGuess") == "yes") && (session.getAttribute("guessResult") == "-1" )){ %>
+			<div class="alert red">
+				<p>Too low!</p>
+			</div>
+		<% } %>
+
+
+		<% if ((session.getAttribute("didGuess") == "yes") && (session.getAttribute("guessResult") == "0" )){ %>
+			<div class="alert green">
+					<p>Yes!  <c:out value="${targetNumber}" /> Is the number</p>
+					
+					<% session.removeAttribute("targetNumber"); %>
+						<% 		session.setAttribute("didGuess", "no"); %>
+						<% 		session.setAttribute("showForm", "no"); %>
+					
+					<form action="" method="GET">
+						<button type="submit" value="Submit">Play Again</button>
+					</form>
+	
+			</div>
+		<% } %>
+
+
+		<% if (session.getAttribute("showForm") == "yes"){ %>
+<form action="" method="POST">
+
+<input type="text" name="inputNumber">
+
+<button type="submit" value="Submit">Submit</button>
+
+</form>
+
+<p>Target number is <c:out value="${targetNumber}"/></p>
+
+			<% } %>	
+		
+</body>
+</html>
---
+++ 03-java-on-the-web/01-java-on-the-web/05-great-number-game/src/com/kenthervey/greatnumber/Number.java
@@ -0,0 +1,119 @@
+package com.kenthervey.greatnumber;
+
+//import java.io.IOException;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Random;
+
+
+
+/**
+ * Servlet implementation class Number
+ */
+@WebServlet("/Number")
+public class Number extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Number() {
+        super();
+        // TODO Auto-generated constructor stub
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();	
+		
+		session.setAttribute("showForm", "yes");
+		
+		
+		
+		int targetNumber = (session.getAttribute("targetNumber")==null? getRandomOne100() : (int)session.getAttribute("targetNumber"));
+		
+		
+		session.setAttribute("targetNumber", targetNumber);
+		
+		//System.out.printf("Target Number is:  %d%n, (int)session.getAttribute('targetNumber')");
+		
+
+		
+		request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+		
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		
+		String redirectLocation = "/GreatNumberGame/Number";
+		
+		String guess = request.getParameter("inputNumber");
+		if(guess=="") {
+			response.sendRedirect(redirectLocation);
+			return;
+		}
+		else {
+			session.setAttribute("didGuess", "yes");
+		}
+		
+		System.out.printf("Printing request InputNumber %s", guess);
+		
+		int guessInt = Integer.parseInt(guess);
+		int targetNumberInt = (int)session.getAttribute("targetNumber");
+		 
+		if(guessInt == targetNumberInt) {
+			System.out.printf("Your guess was correct, %d%n", guessInt);
+			session.setAttribute("guessResult", "0");
+		}
+		else if (guessInt < targetNumberInt) {
+			System.out.printf("Target Number is  %d  ",targetNumberInt);
+			System.out.printf("Your guess %d was too low, %n", guessInt);
+			session.setAttribute("guessResult", "-1");
+		}
+		else {
+			System.out.printf("Target Number is  %d  ",targetNumberInt);
+			System.out.printf("Your guess %d was too High, %n", guessInt);
+			session.setAttribute("guessResult", "1");
+		}
+		
+		
+		
+		
+		
+		response.sendRedirect(redirectLocation);
+		
+	}
+
+	public static int getRandomOne100(){
+		double smallRandom =Math.random();
+		int randomInRange =(int) (smallRandom * (double)(100));
+	    return randomInRange;
+	}
+	
+	
+}
