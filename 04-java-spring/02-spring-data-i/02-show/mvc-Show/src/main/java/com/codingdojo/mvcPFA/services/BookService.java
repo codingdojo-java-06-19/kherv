fa7a6545f283a60394472159d47102392454b0d1
Kent Hervey <kent.hervey@gmail.com>
2019-06-13 20:47:50 -0500

    Show 1st and complete

---
+++ 04-java-spring/02-spring-data-i/02-show/mvc-Show/src/main/java/com/codingdojo/mvcPFA/services/BookService.java
@@ -0,0 +1,70 @@
+package com.codingdojo.mvcPFA.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.mvcPFA.models.Book;
+import com.codingdojo.mvcPFA.repositories.BookRepository;
+
+@Service
+public class BookService {
+	private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll(); //calls method in the Repository
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b); //works because Repository extended the CrudRepository
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    
+	public Book findById(Long id) {
+		Optional<Book> book = bookRepository.findById(id);
+		
+		if (book.isPresent()) {
+			return book.get();
+		}
+		
+		return null;
+	}
+    
+    
+	public Book updateBook(Long id, String title, String desc, String lang, Integer pages) {
+		Book book = findById(id);
+		
+		book.setNumberOfPages(pages);
+		book.setTitle(title);
+		
+		return updateBook(book);
+	}
+	
+	public Book updateBook(Book book) {
+		return bookRepository.save(book);
+	}
+	
+	public void deleteBook(Long id) {
+		bookRepository.deleteById(id);
+	}
+    
+    
+    
+    
+    
+    
+    
+} // end of class
