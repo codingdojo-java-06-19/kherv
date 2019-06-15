6fd8c6b19c636a0392048b5d2a7237423ba2ed2b
Kent Hervey <kent.hervey@gmail.com>
2019-06-15 09:23:45 -0500

    route name change

--- 04-java-spring/02-spring-data-i/04-lookify/Lookify/src/main/java/com/codingdojo/Lookify/controllers/SongsController.java
+++ 04-java-spring/02-spring-data-i/04-lookify/Lookify/src/main/java/com/codingdojo/Lookify/controllers/SongsController.java
@@ -1,9 +1,30 @@
 package com.codingdojo.Lookify.controllers;
 
-public class SongsController {
-	
-	
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codingdojo.Lookify.services.SongService;
+
+
+@Controller
+@RequestMapping("")
+public class SongsController {
+	private final SongService songService;
+	
+	public SongsController(SongService songService) {
+		this.songService = songService;
+	}
+
+	@RequestMapping("/") 
+	public String splash
+	
+	@RequestMapping("/")
+	public String index() {
+		return "redirect:/languages";
+	}
+}
+	
 	
 
 }
