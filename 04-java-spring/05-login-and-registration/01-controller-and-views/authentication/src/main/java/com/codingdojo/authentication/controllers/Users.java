ca85e59f5301bd9c770384a237804513e5ef56bc
Kent Hervey <kent.hervey@gmail.com>
2019-06-20 08:39:09 -0500

    with saves

--- 04-java-spring/05-login-and-registration/01-controller-and-views/authentication/src/main/java/com/codingdojo/authentication/controllers/Users.java
+++ 04-java-spring/05-login-and-registration/01-controller-and-views/authentication/src/main/java/com/codingdojo/authentication/controllers/Users.java
@@ -1,5 +1,56 @@
 package com.codingdojo.authentication.controllers;
 
-public class Users {
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.codingdojo.authentication.models.User;
+import com.codingdojo.authentication.services.UserService;
+
+// imports removed for brevity
+@Controller
+public class Users {
+    private final UserService userService;
+    
+    public Users(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @RequestMapping("/registration")
+    public String registerForm(@ModelAttribute("user") User user) {
+        return "registrationPage.jsp";
+    }
+    @RequestMapping("/login")
+    public String login() {
+        return "loginPage.jsp";
+    }
+    
+    @RequestMapping(value="/registration", method=RequestMethod.POST)
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+        // if result has errors, return the registration page (don't worry about validations just now)
+        // else, save the user in the database, save the user id in session, and redirect them to the /home route
+    }
+    
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
+        // if the user is authenticated, save their user id in session
+        // else, add error messages and return the login page
+    }
+    
+    @RequestMapping("/home")
+    public String home(HttpSession session, Model model) {
+        // get user from session, save them in the model and return the home page
+    }
+    @RequestMapping("/logout")
+    public String logout(HttpSession session) {
+        // invalidate session
+        // redirect to login page
+    }
 }
