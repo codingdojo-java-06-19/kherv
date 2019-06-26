6ee87bcfa0dbdc60d5ac43f51b150ce144de8349
Kent Hervey <kent.hervey@gmail.com>
2019-06-25 20:46:28 -0500

    Two

--- 04-java-spring/05-login-and-registration/02-events-belt-reviewer/EventsBeltReviewTwo/src/main/java/com/codingdojo/EventsBeltReviewTwo/controllers/LoginRegController.java
+++ 04-java-spring/05-login-and-registration/02-events-belt-reviewer/EventsBeltReviewTwo/src/main/java/com/codingdojo/EventsBeltReviewTwo/controllers/LoginRegController.java
@@ -1,5 +1,98 @@
 package com.codingdojo.EventsBeltReviewTwo.controllers;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.codingdojo.EventsBeltReviewTwo.models.User;
+import com.codingdojo.EventsBeltReviewTwo.services.ApiService;
+import com.codingdojo.EventsBeltReviewTwo.validator.UserValidator;
+
+@Controller
 public class LoginRegController {
+	private final ApiService apiService;
+	private final UserValidator userValidator;
+	
+	public LoginRegController(ApiService apiService, UserValidator userValidator) {
+		this.apiService = apiService;
+		this.userValidator = userValidator;
+	}
+	
+	@RequestMapping("/")
+	public String goHome(@ModelAttribute("user") User user) {
+		return "loginReg.jsp";
+	}
+	
+	
+	@PostMapping("/register")
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session, RedirectAttributes redirectAttributes) {
+		System.out.println("here");
+		userValidator.validate(user, result);
+		if(result.hasErrors()) {
+			redirectAttributes.addFlashAttribute("registrationError", "try again");
+			return "redirect:/";
+		}
+		try { 
+			User newUser = apiService.registrationUser(user);
+			//redirectAttributes.addFlashAttribute("preLoginMessage", "try again");
+			return "redirect:/";
+		}
+		catch(Exception exception) {
+			redirectAttributes.addFlashAttribute("registrationError", "try again");
+			return "redirect:/";
+		}
+	}
+	
+	@PostMapping("/login")
+	public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, RedirectAttributes redirectAttributes, HttpSession session) {
+		if(apiService.authenticateUser(email, password)) {
+			User user = apiService.findByEmail(email);
+			session.setAttribute("userId", user.getId());
+			return "redirect:/events/";
+		}
+		redirectAttributes.addFlashAttribute("loginError", "try again");
+		return "redirect:/";
+	}
+	
+	@PostMapping("/logout")
+	public String logout(HttpSession session, RedirectAttributes redirectAttributes) {
+		session.invalidate();
+		redirectAttributes.addFlashAttribute("logoutSuccess", "success");
+		
+		return "redirect:/";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
---
+++ 04-java-spring/05-login-and-registration/02-events-belt-reviewer/EventsBeltReviewTwo/src/main/java/com/codingdojo/EventsBeltReviewTwo/validator/UserValidator.java
@@ -0,0 +1,36 @@
+package com.codingdojo.EventsBeltReviewTwo.validator;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.codingdojo.EventsBeltReviewTwo.models.User;
+
+@Component
+public class UserValidator implements Validator {
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return User.class.equals(clazz);
+	}
+	
+	@Override
+	public void validate(Object target, Errors errors) {
+		User user = (User) target;
+		
+		if(!user.getPasswordConfirmation().equals(user.getPasswordConfirmation())) {
+			errors.rejectValue("passwordConfirmation", "Match");
+			
+		}
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
